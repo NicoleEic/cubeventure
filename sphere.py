7994beaffdf48afbe82594cc0c03e55da810ee18
Nicole Eichert <nicole.eichert@psy.ox.ac.uk>
2020-11-21 16:21:43 +0000

    animation for expanding sphere

---
+++ sphere.py
@@ -0,0 +1,39 @@
+import numpy as np
+import os
+import scipy.ndimage
+from matplotlib import pyplot as plt
+
+
+i_grid = 7
+
+# initialize empty matrix with centre 1
+data_matrix = np.zeros((i_grid, i_grid, i_grid))
+data_matrix = data_matrix[..., np.newaxis]
+vol = np.zeros((i_grid, i_grid, i_grid))
+mid = np.int(np.floor(i_grid/2))
+vol[mid, mid, mid] = 10
+
+# create spheres with various radius
+n_steps = 6
+thres = 0.5
+for sig in np.arange(0, n_steps):
+    vol_s = scipy.ndimage.filters.gaussian_filter(vol, [sig, sig, sig], mode='constant')
+    vol_s = vol_s / np.max(vol_s)
+    vol_s = np.where(vol_s > thres, 1, 0)
+    # append 4D matrix
+    data_matrix = np.append(data_matrix, vol_s[:, :, :, np.newaxis], axis=3)
+
+# flip matrix for decreasing sphere
+data_matrix = np.append(data_matrix, np.flip(data_matrix), axis=3)
+
+### save matrix
+# data path
+dd = os.path.join(os.path.dirname(__file__), 'sequences')
+# output filename
+fname = os.path.join(dd, 'sphere')
+np.save(fname, data_matrix)
+
+# call visualization script
+os.system("python visualisation_demo.py sphere")
+
+
