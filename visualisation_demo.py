e64b7b95f8baf7d7b6a5d5a5ed3034f050abc970
Nicole Eichert <nicole.eichert@psy.ox.ac.uk>
2020-11-21 16:21:00 +0000

    script reads in file

--- visualisation_demo.py
+++ visualisation_demo.py
@@ -1,4 +1,6 @@
 from matplotlib import pyplot as plt
+import os
+import sys
 import numpy as np
 import mpl_toolkits.mplot3d.axes3d as p3
 from matplotlib import animation
@@ -8,27 +10,32 @@ matplotlib.use("TkAgg")
 
 def main():
     i_grid = 7
-    t_step = 200 # milliseconds
+    t_step = 500 # milliseconds
+
+    # initialize 3D scatter plot
     x, y, z = np.meshgrid(np.arange(i_grid), np.arange(i_grid), np.arange(i_grid))
     fig = plt.figure()
     ax = p3.Axes3D(fig)
 
-    n_vols = i_grid**3
-    data_in = np.zeros((i_grid, i_grid, i_grid, n_vols))
-    i_all = 0
-    for i_x in np.arange(i_grid):
-        for i_y in np.arange(i_grid):
-            for i_z in np.arange(i_grid):
-                data_in[:, :, :, i_all] = 0
-                data_in[i_x, i_y, i_z, i_all] = 1
-                i_all = i_all + 1
+    # load 4d matrix from file
+    dd = os.path.join(os.path.dirname(__file__), 'sequences')
+    # default sequence
+    if len(sys.argv) == 1:
+        fname = os.path.join(dd, 'sequence') + '.npy'
+    # if command line argument provided
+    else:
+        fname = os.path.join(dd, sys.argv[1]) + '.npy'
+        if not os.path.isfile(fname):
+            print('sequence does not exist')
+    data_in = np.load(fname)
 
     def update_plot(i):
+        # read in each slice of the 4D matrix
         data_vol = data_in[:, :, :, i]
         # TODO: how to change colours rather than redrawing plot?
         plt.cla()
         scat = ax.scatter(x.flatten(), y.flatten(), z.flatten(), c=data_vol.reshape(-1), cmap='binary',
-                          depthshade=False, vmin=0, vmax=1, edgecolors="black")
+                          depthshade=False, vmin=0, vmax=1, edgecolors="white")
 
         ax.set_xticklabels("")
         ax.set_yticklabels("")
@@ -38,8 +45,17 @@ def main():
         ax.set_zlabel('Z')
         return scat,
 
-    ani = animation.FuncAnimation(fig, update_plot, interval=t_step, frames=data_in.shape[3])
-    plt.show()
+    # create visualization
+    # animation for 4D matrix
+    if len(data_in.shape) == 4:
+        ani = animation.FuncAnimation(fig, update_plot, interval=t_step, frames=data_in.shape[3])
+        plt.show()
+    # static plot if only 3D matrix
+    elif len(data_in.shape) == 3:
+        plt.cla()
+        scat = ax.scatter(x.flatten(), y.flatten(), z.flatten(), c=data_in.reshape(-1), cmap='binary',
+                          depthshade=False, vmin=0, vmax=1, edgecolors="white")
+        plt.show()
 
 
 main()
