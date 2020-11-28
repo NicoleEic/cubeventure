316d9120f0cef7f8e6a70ad99ddc08ddb5ac9019
Nicole Eichert <nicole.eichert@psy.ox.ac.uk>
2020-11-28 17:38:32 +0000

    fix first volume

--- fill_up.py
+++ fill_up.py
@@ -1,17 +1,21 @@
 import numpy as np
 import os
 
-i_grid = 7
+i_grid = 4
 
 data_matrix = np.zeros((i_grid, i_grid, i_grid, i_grid**3))
 
 
-for i in np.arange(0, i_grid**3):
+for i in np.arange(1, i_grid**3):
     long = data_matrix[:, :, :, 0].reshape(i_grid ** 3)
     ind = np.random.choice(np.where(long == 0)[0])
     long[ind] = 1
     back = long.reshape(i_grid, i_grid, i_grid)
     data_matrix[:, :, :, i] = back
+
+# TODO: why does the first volume appear as all lights on?
+# crop out as solution for now
+data_matrix = data_matrix[:, :, :, 1:-1]
 
 ### save matrix
 # data path
