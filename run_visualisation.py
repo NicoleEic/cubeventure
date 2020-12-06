29fdc5ebbb2ec48f07f9d964550958f555823e69
NicoleEic <nicole.eichert@psy.ox.ac.uk>
2020-12-06 12:37:46 +0000

    fix timings

--- run_visualisation.py
+++ run_visualisation.py
@@ -6,33 +6,23 @@ import argparse
 import cubeventure as cv
 
 
-<<<<<<< HEAD
-=======
 class CubeSequence:
     def __init__(self, args):
         try:
             self.args = args
             self.i_grid = args.cube_size
             self.col_pins, self.ly_pins = cv.grid_array(self.i_grid)
-            # Time delay built in between pin output
-            t_p = args.pin_delay
-            # time step between volumes (needs to be multiples of t_p)
-            t_s = args.time_step
-            # number of alterations to code for intensity
-            self.n_steps_pin = 6
-            # repetition of cycles through layers
-            self.n_reps_layer = np.ceil(t_s / (self.n_steps_pin * self.i_grid * t_p)).astype(np.int)
             if len(self.args.matrix) == 0:
-                self.matrix = cv.load_data(self.args.fname)
+                self.matrix = cv.load_matrix(self.args.fname)
             else:
                 self.matrix = self.args.matrix
             self.setup_columns()
             self.show_pattern()
+            #self.single_pin(col=5, layer=23)
         except KeyboardInterrupt:
             GPIO.cleanup()
         finally:
             GPIO.cleanup()
->>>>>>> ed6e364163f4f83eee7addb5c9a20e8182d0bc85
 
     # initialisation of the pins
     def setup_columns(self):
@@ -45,7 +35,7 @@ class CubeSequence:
         GPIO.output(self.ly_pins, False)
 
     # function to turn on a single pin
-    def single_pin(self, col, layer, t_light=5):
+    def single_pin(self, col=13, layer=23, t_light=5):
         GPIO.output(col, True)
         GPIO.output(layer, True)
         sleep(t_light)
@@ -53,13 +43,29 @@ class CubeSequence:
         GPIO.output(layer, False)
 
     def show_pattern(self):
-        # convert intensity values in volume to 4D binary matrix
-        matrix_exp = cv.intensity_to_binary(self.matrix, self.args.n_steps_pin)
-        # loop over repetitions
-        for i_r in np.arange(0, self.n_reps_layer):
-            # loop over elements of expanded matrix
-            for i_v in np.arange(0, matrix_exp.shape[3]):
-                vol = matrix_exp[:, :, :, i_v]
+        # Time delay built in between pin output
+        t_p = self.args.pin_delay
+        # time step between matrix volumes
+        t_s = self.args.time_step
+        
+        if self.matrix.dtype != np.int32:
+            print('convert intensities')
+            # number of binary alterations to code for intensity
+            n_steps_pin = args.n_steps_pin
+            # repetition of cycles through layers
+            n_reps_layer = np.ceil(t_s / (n_steps_pin * self.i_grid * t_p)).astype(np.int)
+            # convert intensities to binary matrix
+            matrix = cv.intensity_to_binary(self.matrix, n_steps_pin)
+        else:
+            matrix = self.matrix
+            n_reps_layer = np.ceil(t_s / (self.i_grid * t_p)).astype(np.int)
+        print(n_reps_layer)
+        
+        # loop over elements of expanded matrix
+        for i_v in np.arange(0, matrix.shape[3]):
+            vol = matrix[:, :, :, i_v]
+            # loop over repetitions
+            for i_r in np.arange(0, n_reps_layer):
                 # loop over layers
                 for i_z in np.arange(0, self.i_grid):
                     # select active columns
@@ -74,6 +80,7 @@ class CubeSequence:
 if __name__ == "__main__":
     print('Press CTRL+C to stop script')
     my_args, unknown = cv.my_parser().parse_known_args()
+    print(my_args)
     if my_args.vis_type == 'cube':
         import RPi.GPIO as GPIO
 
@@ -81,21 +88,9 @@ if __name__ == "__main__":
         cmd_str = f'python visualisation_demo.py --vis_type plot' + cv.args_to_cmd(my_args)
         os.system(cmd_str)
 
-<<<<<<< HEAD
-# mainline logic 
-try:
-    print('Press CTRL+C to stop script')
-    setupColumns()
-    show_pattern(data_in)
-    #test()
- 
-except KeyboardInterrupt:
-    GPIO.cleanup()
-=======
-    elif my_args.vis_type == 'plot':
+    elif my_args.vis_type == 'plot_binary':
         cmd_str = f'python visualisation_demo.py --vis_type plot_binary' + cv.args_to_cmd(my_args)
         os.system(cmd_str)
->>>>>>> ed6e364163f4f83eee7addb5c9a20e8182d0bc85
 
     elif my_args.vis_type == 'cube':
         visu = CubeSequence(my_args)
